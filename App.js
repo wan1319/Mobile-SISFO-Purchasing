7217c32d552fa915dd79eae3d65c95b1ed722a68
wan1319 <117424170+wan1319@users.noreply.github.com>
2023-05-09 21:55:58 +0700

    day1

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
+import ScreenUserLogin from './src/screens/user/ScreenUserLogin';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>SISFO Project</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="ScreenUserLogin" component={ScreenUserLogin}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/user/ScreenUserLogin.js
+++ src/screens/user/ScreenUserLogin.js
@@ -1,5 +1,42 @@
-const ScreenUserLogin = ({ navigation }) => {
+import _ from "lodash";
+import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Text } from "react-native-paper";
 
+const ScreenUserLogin = ({ navigation }) => {
+    // const [, setIsAuthenticated] = useContext(ContextUserAuthentication)
+    const [, setAuthenticator] = useState({});
+    const [complete, setComplete] = useState(false);
+
+    const handleChange = (name, value) => {
+        setUser((values) => ({ ...values, [name]: value }));
+    };
+
+    const useLogin = () => {
+        // TODO: add services
+    };
+
+    useEffect(() => {
+        setComplete(false);
+        const debounce = _.debounce(() => {
+            setComplete(true);
+        }, 500);
+
+        debounce();
+    }, []);
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            {complete && (
+                <ScrollView>
+                    <Text style={{ marginTop: 30 }}>Form Login</Text>
+                </ScrollView>
+            )}
+
+            {!complete && <Text style={{ marginTop: 30 }}>Loading...</Text>}
+        </SafeAreaView>
+    );
 };
 
 export default ScreenUserLogin;
